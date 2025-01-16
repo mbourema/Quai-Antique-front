f37d96fe6cf0e6707ba21de46cd4a5ca1b9381a9
mbourema <mehdibourema@outlook.fr>
2025-01-16 10:27:43 +0100

    Commentaires pour les fonctions

--- js/script.js
+++ js/script.js
@@ -2,27 +2,7 @@ const tokenCookieName = "accesstoken";
 const signOutBtn = document.getElementById("SignoutBtn");
 const roleCookieName = "role";
 
-signOutBtn.addEventListener("click", signout);
-
-function signout(){
-    eraseCookie(tokenCookieName);
-    eraseCookie(roleCookieName);
-    //Recharger la page
-    window.location.reload();
-}
-
-function getRole(){
-    return getCookie(roleCookieName);
-}
-
-function setToken(token){
-    setCookie(tokenCookieName, token, 7);
-}
-
-function getToken(){
-    return getCookie(tokenCookieName);
-}
-
+// Créer un cookie à aprtir de son nom, de sa valeur et de sa durée d'expiration en jours
 function setCookie(name,value,days) {
     var expires = "";
     if (days) {
@@ -33,21 +13,65 @@ function setCookie(name,value,days) {
     document.cookie = name + "=" + (value || "")  + expires + "; path=/";
 }
 
+//Récupérer la valeur d'un cookie à partir de son nom
 function getCookie(name) {
+    //Cette ligne ajoute un signe = au nom du cookie. Par exemple, si le nom du cookie est "user", alors nameEQ devient "user="
     var nameEQ = name + "=";
+    /*document.cookie retourne tous les cookies sous forme d'une chaîne unique, où chaque cookie est séparé par un point-virgule ;
+    Cette ligne divise cette chaîne en un tableau (ca) où chaque élément est un cookie.
+    */
     var ca = document.cookie.split(';');
+    //La boucle for parcourt chaque cookie dans le tableau ca.
     for(var i=0;i < ca.length;i++) {
         var c = ca[i];
+        /*Certains cookies peuvent avoir des espaces en début de chaîne.
+        Cette boucle while supprime les espaces en début de la chaîne pour assurer une correspondance correcte.*/
         while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        /*c.indexOf(nameEQ) vérifie si le cookie commence par la chaîne nameEQ (le nom du cookie suivi de =)
+        Si c'est vrai, cela signifie que le cookie recherché a été trouvé.
+        Ensuite, on extrait la valeur du cookie en utilisant c.substring(nameEQ.length, c.length), qui commence juste après name= et prend tout le reste de la chaîne. */
         if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+        //Si aucun cookie avec le nom donné n'est trouvé, la fonction retourne null.
     }
     return null;
 }
 
-function eraseCookie(name) {   
+//Création du cookie jeton
+function setToken(token){
+    setCookie(tokenCookieName, token, 7);
+}
+
+//Obtenir la valeur du jeton de connexion
+function getToken(){
+    return getCookie(tokenCookieName);
+}
+
+//Obtenir la valeur du cookie role
+function getRole(){
+    return getCookie(roleCookieName);
+}
+
+// Supprimer un cookie à partir de son nom
+function eraseCookie(name) {  
+    /* name + '=': Cette partie spécifie le nom du cookie suivi d'une valeur vide (=). Cela réécrit le cookie avec une valeur vide. 
+    Path=/: Cela indique que le cookie est défini pour la racine (/) du domaine. Cela garantit que le cookie sera supprimé à tous les niveaux du site.
+    Expires=Thu, 01 Jan 1970 00:00:01 GMT;: Cette partie définit une date d'expiration dans le passé (1er janvier 1970).
+    Les cookies expirés sont automatiquement supprimés par le navigateur.*/
     document.cookie = name +'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
 }
 
+// Fonction pour gérer la déconnexion à l'aide de la suppression du cookie token et du cookie role
+function signout(){
+    eraseCookie(tokenCookieName);
+    eraseCookie(roleCookieName);
+    //Recharger la page
+    window.location.reload();
+}
+
+// Bouton de déconnexion
+signOutBtn.addEventListener("click", signout);
+
+// Fonction qui vérifie si on est connexté sur le site par la présence ou non du cookie jeton
 function isConnected(){
     if(getToken() == null || getToken == undefined){
         return false;
@@ -58,11 +82,17 @@ function isConnected(){
 }
 
 function showAndHideElementsForRoles(){
+    //Récupération du booleen qui dit si l'utilisateur est connecté ainsi que de la valeur du cookie role
     const userConnected = isConnected();
     const role = getRole();
 
+    //Sélection de tout les attributs data-show de toutes les pages du projet
     let allElementsToEdit = document.querySelectorAll('[data-show]');
 
+    /* Switch case, si la valeur du data-show est 'disconnected' et que l'utilisateur est connecté l'élément est
+    masqué, si la valeur est 'connected' et que l'utilisateur n'est pas connecté l'élément est masqué,
+    si la valeur est admin et que l'utilisateur n'est pas connecté ou que son rôle n'est pas admin l'élément est masqué,
+    si la valeur est client et que l'utilisateur n'est pas connecté ou que son rôle n'est pas client l'élément est masqué*/
     allElementsToEdit.forEach(element =>{
         switch(element.dataset.show){
             case 'disconnected': 
--- js/signin.js
+++ js/signin.js
@@ -5,18 +5,23 @@ const btnSignin = document.getElementById("btnSignin");
 
 btnSignin.addEventListener("click", checkCredentials);
 
+// Fonction pour gérer la connexion pour la page de connexion
 function checkCredentials(){
     //Ici, il faudra appeler l'API pour vérifier les credentials en BDD
-    
+    //Si la valeur du champ e-mail est 'test@mail.com' et que la valeur du champ mot de passe est '123'
     if(mailInput.value == "test@mail.com" && passwordInput.value == "123"){
         //Il faudra récupérer le vrai token
         const token = "lkjsdngfljsqdnglkjsdbglkjqskjgkfjgbqslkfdgbskldfgdfgsdgf";
+        /* Un cookie est créé avec le nom "accesstoken" et la valeur de la constante token et 
+        une durée d'expiration de 7 jours (en utilisant la fonction setCookie) */
         setToken(token);
-        //placer ce token en cookie
+        //L'utilisateur est redirifé dans la page d'accueil
         window.location.replace("/");
+        //Un cookie avec le role d'administrateur est créé pour 7 jours
         setCookie(roleCookieName, "admin", 7);
     }
     else{
+        //Sinon les champs du formulaire de connexion s'entourent de rouge
         mailInput.classList.add("is-invalid");
         passwordInput.classList.add("is-invalid");
     }
